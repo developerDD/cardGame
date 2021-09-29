3bf6d6f2adc811803ff4f7798774a843c6657a47
dimaderevanko <developerDD@ukr.net>
2021-09-29 21:06:02 +0300

    add label count

--- caredGameForMemory/ViewController.swift
+++ caredGameForMemory/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var flipCount = 0
     
+    @IBOutlet weak var flipCountLable: UILabel!
     @IBAction func touchCard(_ sender: UIButton) {
-        print("Hi")
+        flipCount+=1
+        flipCountLable.text = "Flips: \(flipCount)"
+        flipCard(withEmoji: "🐭", on: sender)
+    }
+    @IBAction func touchSecodCard(_ sender: UIButton) {
+        flipCount+=1
+        flipCountLable.text = "Flips: \(flipCount)"
+        flipCard(withEmoji: "🦊", on: sender)
     }
     
-
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+       // print("Flip card \(emoji)")
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = .orange
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = .white
+        }
+        
+    }
 }
 
